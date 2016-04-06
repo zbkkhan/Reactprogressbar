f280bddccb0484ee24029e28ed4c81b992b289bc
Khutaja Qureshi <Khutaja Qureshi>
2016-04-06 11:52:33 -0400

    temp

---
+++ progressbar/js/app.js
@@ -0,0 +1,69 @@
+var ProgressBar = React.createClass({
+    
+    getInitialState: function(){
+        return {progress: 0}//progress is counted in percentile
+    },
+    handleChange: function(){
+
+        if(this.state.progress <= 100) {
+            var updateProgress = this.state.progress;
+            updateProgress = updateProgress + 10;
+            this.setState({progress: updateProgress});
+        }
+        else{
+            this.setState({progress: 100});
+        }
+    },
+    componentDidMount: function(){//runs after initial mount
+        var updateProgress=this.state.progress + 10;
+        this.setState({progress:updateProgress});
+    },
+    componentDidUpdate: function(){//runs every update
+        var $this= this;
+
+       var timer= setInterval(function(){
+            if($this.state.progress<100){
+                $this.handleChange()
+            }
+           else{
+                clearInterval(timer);
+            }
+            },2000
+        );
+
+    },
+    render: function(){
+        var progress = this.state.progress;
+        var innerDivStyle= {
+            backgroundColor: 'pink',
+            width: progress+'%'
+
+        };
+        var outerDivStyle={
+          borderStyle: 'solid'
+
+        };
+        if(this.state.progress < 100){
+
+            return(
+                <div className="col-md-6" style={outerDivStyle} >
+                    <div style={innerDivStyle} >{this.state.progress}%</div>
+
+                </div>
+            );
+        }
+        else{
+            return(
+                <h1>Thank you for downloading with Loudtronix:)</h1>
+            );
+        }
+
+    }
+    
+
+});
+
+
+React.render(<ProgressBar/>,
+    document.getElementById('react-container'));
+
---
+++ searchbar/js/app.js
@@ -0,0 +1,50 @@
+var SearchResult = React.createClass({
+
+    getInitialState: function(){
+        return {typing: false , value: ""}
+    },
+
+    handleChange: function(event){
+        this.setState({typing: true, value: event.target.value})
+    },
+    render: function(){
+        var search = this.state.value;
+        search = search.toLowerCase();
+        var songName =  this.props.songs.map(function(song) {
+            
+            var re = new RegExp(search, 'g');
+            var songTitle= song.title.toLowerCase();
+            if( songTitle.match(re) == search){
+                
+               return (
+                       <a href={song.link}> <h3>{song.title}</h3> </a>
+                        
+                    )
+            }
+            
+      
+    });
+       
+
+
+      return(<div>
+
+          <form><input type="text" placeholder="Search" value={this.state.value} onChange={this.handleChange}/></form>
+             
+              {songName}
+          </div>);
+    }
+
+});
+
+var SONGS = [
+    {title: 'Drake- Hotline Bling', Duration: '3:00', link: "https://www.youtube.com/watch?v=uxpDa-c-4Mc"},
+     {title: 'Drake- Hotline Bling', Duration: '3:00', link: "https://www.youtube.com/watch?v=uxpDa-c-4Mc"},
+     {title: 'Drake- Hotline Bling', Duration: '3:00', link: "https://www.youtube.com/watch?v=uxpDa-c-4Mc"},
+     {title: 'Drake- Hotline Bling', Duration: '3:00', link: "https://www.youtube.com/watch?v=uxpDa-c-4Mc"},
+    {title: 'Drake- Right Hand', Duration: '3:00', link: "https://www.youtube.com/watch?v=NSG-Ggn6Z_c"},
+    {title: 'Selena Gomez - Come and Get it', Duration: '3:00', link: "https://www.youtube.com/watch?v=n-D1EB74Ckg"},
+    {title: 'Tony Igy- Astronomia', Duration: '3:00', link: "https://www.youtube.com/watch?v=4Vk5DpS8hmY"}
+];
+React.render(<SearchResult songs={SONGS}/>,
+    document.getElementById('react-container'));
